ca84f90d91fe3fc2aa1bc6c01d33d59b80e75ea2
Daniel Antunes <antunesdaniel083@gmail.com>
2024-01-30 12:07:22 -0300

    criando os form

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from contact.models import Contact
-from django.db.models import Q
-from django.http import Http404
+from contact.forms import ContactForm
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
 
     return render(
         request,
